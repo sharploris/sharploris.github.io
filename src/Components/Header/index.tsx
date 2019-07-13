691bf54c5604fc1c455b717fe52811c5690c9756
Daniel Williams <danielw093@gmail.com>
2019-07-13 19:03:17 +0100

    Formatting tweaks

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,14 +3,14 @@ import logo from '../../img/lorisLogo.png';
 import styles from './index.module.scss';
 
 export default class Header extends Component {
-    render() {
-      return (
-        <header className={styles.headerBar}>
-            <img src={logo} height="50" className={styles.logo} />
-            <span className={styles.headerText}>
-                Sharp Loris Games
-            </span>
-        </header>
-      );
-    }
+  render() {
+    return (
+      <header className={styles.headerBar}>
+        <img src={logo} height="50" className={styles.logo} alt="" />
+        <span className={styles.headerText}>
+          Sharp Loris Games
+        </span>
+      </header>
+    );
   }
+}
