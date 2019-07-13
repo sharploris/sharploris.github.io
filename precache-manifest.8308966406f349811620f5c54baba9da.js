442d4381e3d3b37b68d5ada111184ffcc73a50ee
Daniel Williams <danielw093@gmail.com>
2019-07-13 22:01:07 +0100

    Updates

---
+++ precache-manifest.8308966406f349811620f5c54baba9da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5123f7411c925f9d95c33c5d05c5c6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6142af26f635cf3df304",
+    "url": "/static/css/main.d02b97fd.chunk.css"
+  },
+  {
+    "revision": "cf17c4d59262e482316e",
+    "url": "/static/js/2.5b363b31.chunk.js"
+  },
+  {
+    "revision": "6142af26f635cf3df304",
+    "url": "/static/js/main.405b7863.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "46c03615f552037e674e403a111f286f",
+    "url": "/static/media/lorisLogo.46c03615.png"
+  }
+]);
