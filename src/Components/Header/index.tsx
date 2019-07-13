5920dea416a403b7c27eb5a33c7677acecea31f1
Daniel Williams <danielw093@gmail.com>
2019-07-13 18:41:02 +0100

    Created header component

---
+++ src/Components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import logo from '../../img/lorisLogo.png';
+import './index.css';
+
+export default class Header extends Component {
+    render() {
+      return (
+        <header className="header-bar">
+            <img src={logo} height="50" className="logo" />
+            <span className="header-text">
+                Sharp Loris Games
+            </span>
+        </header>
+      );
+    }
+  }
