d0ced2204b22d908e25ed22cf5c13e5bdda2a042
Daniel Williams <danielw093@gmail.com>
2019-07-13 20:12:48 +0100

    Reproduced old site in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.scss';
+import styles from './App.module.scss';
 import Header from './Components/Header';
+import PeoplePage from './Components/PeoplePage'
 
 const App: React.FC = () => {
   return (
     <div>
-      <Header></Header>
+      <Header />
+      <div className={`${styles.appBody} centered`}>
+        <h2>Met us at Develop:Brighton? Reach out!</h2>
+        <br />
+        <PeoplePage />
+      </div>
     </div>
   );
 }
