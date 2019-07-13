75cc4d2dc4029851800a11a68a574704539d11f6
Daniel Williams <danielw093@gmail.com>
2019-07-13 20:27:35 +0100

    Minor improvements

--- src/Components/PeoplePage/index.tsx
+++ src/Components/PeoplePage/index.tsx
@@ -1,40 +1,45 @@
 import React, { Component } from "react";
 import Profile from '../Profile';
+import styles from './index.module.scss';
 
 export default class PeoplePage extends Component {
   render() {
     return (
       <div>
-        <Profile
-          personName="Daniel Williams"
-          twitterUrl="https://twitter.com/DanielW094"
-          linkedinUrl="https://www.linkedin.com/in/daniel-williams-4295b8a8"
-          githubUrl="https://github.com/DanielW093"
-          contactEmail="contact@sharploris.net"
-        />
-        <h4>Programmer on <a href="https://twitter.com/Oiseaudev" target="_blank" rel="noopener noreferrer">Oiseau</a></h4>
-        <h4>Developer of the Loris Settings Manager editor extension for Unity3D</h4>
-        <p>
-          <a href="https://sharploris.net/LorisSettingsManager" target="_blank" rel="noopener noreferrer">
-            Find the documentation here
-          </a>
-          &nbsp;and the&nbsp;
-          <a href="https://assetstore.unity.com/packages/tools/utilities/loris-settings-manager-139326" target="_blank" rel="noopener noreferrer">
-            asset store page here.
-          </a>
-        </p>
-        <h4>Software Engineer at <a href="https://www.madetech.com/" target="_blank" rel="noopener noreferrer">Made Tech</a></h4>
+        <div className={styles.profileWrapper}>
+          <Profile
+            personName="Daniel Williams"
+            twitterUrl="https://twitter.com/DanielW094"
+            linkedinUrl="https://www.linkedin.com/in/daniel-williams-4295b8a8"
+            githubUrl="https://github.com/DanielW093"
+            contactEmail="contact@sharploris.net"
+          />
+          <h4>Programmer on <a href="https://twitter.com/Oiseaudev" target="_blank" rel="noopener noreferrer">Oiseau</a></h4>
+          <h4>Developer of the Loris Settings Manager editor extension for Unity3D</h4>
+          <p>
+            <a href="https://sharploris.net/LorisSettingsManager" target="_blank" rel="noopener noreferrer">
+              Find the documentation here
+            </a>
+            &nbsp;and the&nbsp;
+            <a href="https://assetstore.unity.com/packages/tools/utilities/loris-settings-manager-139326" target="_blank" rel="noopener noreferrer">
+              asset store page here.
+            </a>
+          </p>
+          <h4>Software Engineer at <a href="https://www.madetech.com/" target="_blank" rel="noopener noreferrer">Made Tech</a></h4>
+        </div>
         <br />
         <br />
-        <Profile
-          personName="Jess Molloy"
-          twitterUrl="https://twitter.com/jessabellerina"
-          linkedinUrl="https://www.linkedin.com/in/jessmolloyga/"
-          contactEmail="jessmolloyga@gmail.com"
-        />
-        <h4>Design, accessibility and 3D art on <a href="https://twitter.com/Oiseaudev" target="_blank" rel="noopener noreferrer">Oiseau</a></h4>
-        <h4>Developer of <a href="https://jessmolloyga.wixsite.com/jessmolloy" target="_blank" rel="noopener noreferrer">The Green Monster</a> mod for Fallout 4</h4>
-        <h4>Games Development (Design) Masters Student at Kingston University</h4>
+        <div className={styles.profileWrapper}>
+          <Profile
+            personName="Jess Molloy"
+            twitterUrl="https://twitter.com/jessabellerina"
+            linkedinUrl="https://www.linkedin.com/in/jessmolloyga/"
+            contactEmail="jessmolloyga@gmail.com"
+          />
+          <h4>Design, accessibility and 3D art on <a href="https://twitter.com/Oiseaudev" target="_blank" rel="noopener noreferrer">Oiseau</a></h4>
+          <h4>Developer of <a href="https://jessmolloyga.wixsite.com/jessmolloy" target="_blank" rel="noopener noreferrer">The Green Monster</a> mod for Fallout 4</h4>
+          <h4>Games Development (Design) Masters Student at Kingston University</h4>
+        </div>
       </div>
     );
   }
--- src/Components/Profile/index.tsx
+++ src/Components/Profile/index.tsx
@@ -12,21 +12,21 @@ interface IProfileProps {
 export default class Profile extends Component<IProfileProps, {}> {
     render() {
       return (
-        <div >
+        <div>
           <h3>{this.props.personName}</h3>
-          <div>
+          <span className={styles.socialLinks}>
             {this.props.twitterUrl && this.renderLink("Twitter", this.props.twitterUrl, "mdi mdi-twitter")}
             {this.props.linkedinUrl && this.renderLink("LinkedIn", this.props.linkedinUrl, "mdi mdi-linkedin")}
             {this.props.githubUrl && this.renderLink("GitHub", this.props.githubUrl, "mdi mdi-github-circle")}
             {this.props.contactEmail && this.renderLink("Email", `mailto:${this.props.contactEmail}`, "mdi mdi-email-outline")}
-          </div>
+          </span>
         </div>
       );
     }
 
     private renderLink(accessibilityName: string, url: string, iconName: string){
       return (
-        <a href={url} target="_blank" rel="noopener noreferrer" className={`${styles.icon} ${iconName} fa-4x`}>
+        <a href={url} target="_blank" rel="noopener noreferrer" className={`${styles.icon} ${iconName}`}>
           <span className="hidden-text">
             {accessibilityName}
           </span>
